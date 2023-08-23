e0ea9c2700f11a261ae3f3cf7f56b82f114203ed
Yash Samal <samaljyotiraditya@gmail.com>
2023-08-23 11:24:12 +0530

    Added more details

--- src/component/TempApp.js
+++ src/component/TempApp.js
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from "react";
 import "./TempApp.css";
 
 const TempApp = () => {
-  const [city, setCity] = useState(null);
+  const [city, setCity] = useState("");
   const [search, setSearch] = useState("Mumbai");
+  const [allData, setAllData] = useState();
 
   useEffect(
     function () {
@@ -12,8 +13,9 @@ const TempApp = () => {
         const response = await fetch(url);
         //   console.log(response);
         const jsondata = await response.json();
-        //   console.log(jsondata);
+        console.log(jsondata);
         setCity(jsondata.main);
+        setAllData(jsondata);
       };
       fetchApi();
     },
@@ -25,6 +27,7 @@ const TempApp = () => {
       <div className="inputinfo">
         <input
           type="search"
+          placeholder="Enter a city"
           value={search}
           className="inputfield"
           onChange={(e) => {
@@ -34,14 +37,26 @@ const TempApp = () => {
       </div>
 
       {!city ? (
-        <p>No Data Found</p>
+        <p className="location-info">No Data Found</p>
       ) : (
-        <div className="locationinfo">
+        <div className="location-info">
           <h2 className="location">{search}</h2>
           <h1 className="temp">{city.temp}°Cel</h1>
+
           <h3>
             Min:- {city.temp_min}°Cel | Max:- {city.temp_max}°Cel
           </h3>
+          <h4 style={{ color: "" }}>Feels Like: {city.feels_like}°Cel</h4>
+          <div className="flex-fix">
+            <div>
+              <h3>Weather: {allData.weather[0].description}</h3>
+              <h3>Humidity: {allData.main.humidity}%</h3>
+            </div>
+            <div>
+              <h3>Wind: {allData.wind.speed * 3.6} Kmph</h3>
+              <h3>Visibility: {allData.visibility / 1000} Km</h3>
+            </div>
+          </div>
         </div>
       )}
     </div>
